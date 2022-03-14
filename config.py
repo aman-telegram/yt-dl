f5f97889765ed493639f975298fa2250cb6cd13f
aman-telegram <100060508+aman-telegram@users.noreply.github.com>
2022-03-14 20:55:17 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+BOT_TOKEN = "2068487579:AAGpu6QElHJ3SbRsd1gLilevrunygtnMBdA"
+APP_ID = 14584298
+API_HASH = "77bbda20c5aaae67c7c054df4829968e"
+
+youtube_next_fetch = 0  # time in minute
+
+
+EDIT_TIME = 5
